e3851700cf1d5b220f1507e90604e377fe43bd97
Tomasz Kuwlciki <gram.bfh3@gmail.com>
2020-10-14 13:39:27 +0200

    Dodanie guzika, dzialalnosc js

--- Cw1 lighbox/main.js
+++ Cw1 lighbox/main.js
@@ -1,4 +1,10 @@
 const images = document.querySelectorAll('.gallery img');
+const lightbox = document.querySelector('.lightbox');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const zamknij = document.querySelector('#zamknij');
+let prevImg;
+let nextImg;
 
 for (let idx = 0; idx < images.length; idx++) {
     const img = images[idx];
@@ -6,11 +12,44 @@ for (let idx = 0; idx < images.length; idx++) {
 }
 
 function showLightbox(ev) {
-    console.log(ev.target);
-    const lightbox = document.querySelector('.lightbox');
-    const img = document.querySelector('.lightbox img');
+    console.log('elo');
+    const img = document.querySelector('#obrazeklight');
     const imgUrl = ev.target.src;
     img.src = imgUrl;
     lightbox.classList.add('visible');
-    console.log('showlight');
+    prevImg = ev.target.previousElementSibling;
+    nextImg = ev.target.nextElementSibling;
 }
+
+zamknij.addEventListener('click',hideLightbox);
+function hideLightbox() {
+    lightbox.classList.remove('visible');
+}
+
+
+
+next.addEventListener('click', function () 
+{ 
+    hideLightbox();
+    if(nextImg){
+        nextImg.click();
+    }
+    else
+    { 
+        images[0].click();  // sprawdzenie czy obecne zdjecie jest ostatnim  w kolejnosci, jesli tak to nastepuje zawiniecie
+    }
+}
+);
+
+prev.addEventListener('click', function () 
+{
+    hideLightbox();
+    if(prevImg){
+        prevImg.click();
+    }
+    else
+    { 
+        images[images.length - 1].click();  // sprawdzenie czy obecne zdjecie jest pierwszym  w kolejnosci, jesli tak to nastepuje zawiniecie
+    }
+}
+);
