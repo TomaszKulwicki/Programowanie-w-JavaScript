25d8638c3d09fe538d902b16a289cc198fe69a06
Tomasz Kuwlciki <gram.bfh3@gmail.com>
2020-11-08 16:24:44 +0100

    Praca w js

--- WSEI/Cw3 note/main.js
+++ WSEI/Cw3 note/main.js
@@ -1,9 +1,57 @@
-const btn = document.querySelector('#dodawanie');
+const notebtn = document.querySelector('#dodawanie');
+const NoteContainer = document.querySelector('main');
+const lskey = 'notes';
 
-btn.addEventListener('click', createNote);
+const notes = [];
 
-function createNote() {
-    const div = document.createElement('div');
-    document.body.appendChild(div);
-    div.classList.add('notatka');
+const note = {
+    tytul: '',
+    tresc: '',
+    createDate: new Date(),
+};
+
+localStorage.setItem(lskey, JSON.stringify(notes));
+const notesFromLocalStorage = JSON.parse(localStorage.getItem(lskey));
+console.log(notesFromLocalStorage);
+
+const mappedNotes = notesFromLocalStorage.map( note => {
+    note.createDate = new Date(note.createDate);
+    return note;
+});
+
+notebtn.addEventListener('click', budowanie);
+
+function budowanie(){
+    const htmlNote = document.createElement('div');
+    const htmlTitle = document.createElement('h1');
+    const htmlContent = document.createElement('p');
+    const htmlDate = document.createElement('h4');
+    const htmlRemBtn = document.createElement('button');
+    
+    htmlTitle.innerHTML = note.title;
+    htmlContent.innerHTML = note.content;
+    htmlDate.innerHTML = note.createDate.toDateString();
+    htmlRemBtn.innerHTML = 'Skasuj';
+
+    
+    htmlNote.classList.add('notatka');
+    htmlNote.appendChild(htmlTitle);
+    htmlNote.appendChild(htmlContent);
+    htmlNote.appendChild(htmlDate);
+    htmlNote.appendChild(htmlRemBtn);
+
+    NoteContainer.appendChild(htmlNote);
+
+    htmlRemBtn.addEventListener('click', kasowanie);
+
+    function kasowanie() {
+        NoteContainer.removeChild(htmlNote);
+    }
+}
+notebtn.addEventListener('click', onNewNote);
+
+function onNewNote() {
+    const title = document.querySelector('#tytul').value;
+    const content = document.querySelector('#tresc').value;
+    console.log(title,content);
 }
