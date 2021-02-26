ed3e86c33633763bf6e5fd441b2827dc95dcf604
Tomasz Kuwlciki <gram.bfh3@gmail.com>
2021-02-26 14:45:23 +0100

    Commit po egzaminie

---
+++ WSEI/1. Lighbox/main.js
@@ -0,0 +1,52 @@
+const images = document.querySelectorAll('.gallery img');
+const lightbox = document.querySelector('.lightbox');
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const zamknij = document.querySelector('#zamknij');
+let prevImg;                                                    //adresy sasiednich zdjec
+let nextImg;    
+
+for (let i = 0; i < images.length; i++) {
+    const img = images[i];
+    img.addEventListener('click', showLightbox);
+}
+
+function showLightbox(ev) {
+    const img = document.querySelector('#obrazeklight');
+    const imgUrl = ev.target.src;
+    img.src = imgUrl;
+    lightbox.classList.add('visible');
+    prevImg = ev.target.previousElementSibling;             //przypisywanie adresow sasiednich zdjec
+    nextImg = ev.target.nextElementSibling;                 //
+}
+
+zamknij.addEventListener('click',hideLightbox);
+function hideLightbox() {
+    lightbox.classList.remove('visible');
+}
+
+next.addEventListener('click', function ()
+{ 
+    hideLightbox();
+    if(nextImg){            
+        nextImg.click();                                    //click powoduje pojawienie sie lightboxa
+    }
+    else
+    { 
+        images[0].click();                                  // sprawdzenie czy obecne zdjecie jest ostatnim  w kolejnosci, jesli tak to nastepuje zawiniecie
+    }
+}
+);
+
+prev.addEventListener('click', function () 
+{
+    hideLightbox();
+    if(prevImg){
+        prevImg.click();
+    }
+    else
+    { 
+        images[images.length - 1].click();              // sprawdzenie czy obecne zdjecie jest pierwszym  w kolejnosci, jesli tak to nastepuje zawiniecie do ostatniego
+    }
+}
+);
